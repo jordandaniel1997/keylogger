6200d64b027304830a02b93706402c0b52165a05
Daniel Romero Linux <jordandaniel110814@gmail.com>
2024-09-20 15:29:21 -0600

    fix: add new branch refactor

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import keyboard
 
+def pos_cursor_map(dict_props:dict, cursor_to_map:str)->bool:
+    start_index_right = -1
+    start_index_left = 0
+    pos_cursor_right, pos_cursor_left = dict_props.values()
+
+    match cursor_to_map:
+        case "right":
+            if pos_cursor_right == start_index_right:
+                return True
+            else:
+                return False
+        case "left":
+            pass
+
+
 def space_key(word_lst, index_dict)->None:
     space = ' '
-    
+    #TODO: Cambiar la repeticion de esta validacion
     if index_dict["index_negative"]["index_right"] == index_dict["index_negative"]["start_index"]:
         word_lst.append(space)
     else:
         word_lst.insert(index_dict["index_negative"]["index_right"] + 1, space)
 
 def back_delete_key(word_lst:list, index_dict:dict)->None: 
+    #TODO: Cambiar la repeticion de esta validacion
     if index_dict["index_negative"]["index_right"] == index_dict["index_negative"]["start_index"]:
         word_lst.pop()
     else:
@@ -17,18 +33,17 @@ def back_delete_key(word_lst:list, index_dict:dict)->None:
     print(word_lst)
     
 #FIXME: Reparar funcion de flecha izquierda cuando el foco se encuentra al inicio
-def arrow_left(word_lst, index_dict)->None:
-    length_word = len(word_lst)
-    
-    index_dict["index_negative"]["index_right"] += -1
-    index_dict["index_default"]["index_left"] += 1
-    
+def arrow_left(word_lst, cursor)->None:
+    #length_word = len(word_lst)
+
+    cursor["pos_right"] += -1
+
 #FIXME: Reparar funcion de flecha derecha cuando el foco se encunentra al final
 def arrow_right(index_dict)->None:
     index_dict["index_default"]["index_left"] += 1
     index_dict["index_negative"]["index_right"] -= -1
         
-def key_handler(key:str, word_lst:list, index_dict:dict)->None:
+def key_handler(key:str, word_lst:list, cursor:dict)->None:
     '''
     Esta función gestiona las teclas presionadas por el usuario
     en base a eso, ejecuta cierta función dependiendo de la tecla
@@ -45,43 +60,35 @@ def key_handler(key:str, word_lst:list, index_dict:dict)->None:
                 back_delete_key(word_lst, index_dict)
                 
             case 'flecha izquierda':
-                arrow_left(word_lst, index_dict)
-                print(index_dict)
-                print(word_lst[index_dict["index_negative"]["index_right"]])
-                print(word_lst[index_dict["index_default"]["index_left"]])
-                
+                #arrow_left(word_lst, cursor)
+                print(True)
+                print(cursor)
             case 'flecha derecha':
                 arrow_right(index_dict)
                 print(index_dict["index_negative"]["index_right"])
-                print(index_dict["index_default"]["index_left"])                  
+                print(index_dict["index_default"]["index_left"])
     else:
         """_summary_
         El flujo de programa entra aquí si se presiona una letra: a - z
         """
-        if index_dict["index_negative"]["index_right"] == index_dict["index_negative"]["start_index"]:
+
+        #FIXME: Reparar la invocación de la funcione pos_cursor_map
+        if pos_cursor_map(cursor, "right"):
             word_lst.append(key)
             print(word_lst)
         else:
-            word_lst.insert(index_dict["index_negative"]["index_right"] + 1, key)
+            word_lst.insert(cursor["pos_right"] + 1, key)
             print(word_lst)
 
 def main():
     word_list = []
     void_str = ''
-    indexes = {
-        "index_negative": {
-            "start_index": -1,
-            "index_right": None
-        },
-        "index_default": {
-            "start_index": 0,
-            "index_left": None
-        }
+    cursor = {
+        "pos_right": -1,
+        "pos_left": 0
     }
-    indexes['index_negative']["index_right"] = indexes['index_negative']["start_index"]
-    indexes['index_default']["index_left"] = indexes['index_default']["start_index"]
     
-    key_press = lambda k: key_handler(k.name, word_list, indexes)
+    key_press = lambda k: key_handler(k.name, word_list, cursor)
     keyboard.on_press(key_press, suppress=False)
     
     # Mantiene la ejecución del script
@@ -89,7 +96,6 @@ def main():
         if keyboard.is_pressed('esc'): break
     
     print(void_str.join(word_list))
-    
 
 if __name__ == "__main__":
     main()
